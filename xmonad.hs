3dae04258049cf21619dd81bce6824be950cf5f5
Chavão <fale@chavao.net>
2013-05-16 17:36:20 -0700

    Setup dzen2 statusBar

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,16 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 
-main = xmonad $ myConfig
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+myBar = "dzen2 -p -xs 1 -ta l -e 'onstart=lower'"
+myPP = dzenPP {
+    ppHidden = pad
+    , ppTitle = pad . dzenColor "#bbb" "" . dzenEscape
+    , ppLayout = const ""
+}
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = defaultConfig {
     terminal = "terminator -T Terminator"
